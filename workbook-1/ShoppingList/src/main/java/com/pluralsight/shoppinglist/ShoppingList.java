0b6ac10c5a14d37e97250a11fc3ded27c63725f8
Jermaine Eggleston <gaga3630@gmail.com>
2024-09-26 10:27:55 -0400

    Gotta get these to eat

--- workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
@@ -4,6 +4,7 @@ public class ShoppingList {
 
     public static void main(String[] args){
         System.out.println("Buy stuff these things!:");
+        //Have to get items for the house
         System.out.println("potatoes, milk, eggs, cereal, bacon, steak, bread, clothes, hot pockets, and paper towels");
 
 
