8731c99f01acec1a8431355ffcef10fda60c4d72
Jermaine Eggleston <gaga3630@gmail.com>
2024-09-26 10:15:38 -0400

    Hello World

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight.shoppinglist;
+
+public class ShoppingList {
+
+    public static void main(String[] args){
+        System.out.println("Buy stuff these things!:");
+        System.out.println("potatoes, milk, eggs, cereal, bacon, steak, bread, clothes, hot pockets, and paper towels");
+
+
+    }
+}
---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //hello world will print in terminal
+        System.out.println("Hello World");
+    }
+
+}
+
+
